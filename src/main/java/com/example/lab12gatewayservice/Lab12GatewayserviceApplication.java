9382e9ec81152c410a0148020f8b94119480019f
63070080 <73686422+63070080@users.noreply.github.com>
2022-11-08 20:15:53 +0700

    d

---
+++ src/main/java/com/example/lab12gatewayservice/Lab12GatewayserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.lab12gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Lab12GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab12GatewayserviceApplication.class, args);
+	}
+
+}
