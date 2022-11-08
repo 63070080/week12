3f2a26b5a58ead98fdf30465a5b353cd72332995
63070080 <73686422+63070080@users.noreply.github.com>
2022-11-08 20:16:36 +0700

    ew

---
+++ src/main/java/com/example/lab12authservice/Lab12AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab12authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab12AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab12AuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab12authservice/Lab12AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab12authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab12AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
